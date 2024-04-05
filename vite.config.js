cbab6c3e547aeea5261726cb782e3174a0878a0f
Manish <125258789+ommanish@users.noreply.github.com>
2024-04-05 16:16:52 -0700

    config pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/quiz-app/",
   plugins: [react()],
   build: {
     outDir: "dist", // Change 'build' to 'dist' or your desired directory name
